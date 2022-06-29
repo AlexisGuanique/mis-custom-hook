af98ab2de4a6c35d7f6f2e19496229eaa943a495
Alexis Guanique <guaniqued@gmail.com>
2022-06-29 02:43:24 -0300

    modificacion al useForm

--- useForm/useForm.js
+++ useForm/useForm.js
@@ -1,33 +1,25 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
-// Este es un custom hook que se va a encargar de manejar los formularios
-export const useForm = (initialState = {}) => {
+export const useForm = ( initialForm = {} ) => {
+  
+    const [ formState, setFormState ] = useState( initialForm );
 
-    // Definimos el estado inicial de nuestro componente
-    const [ values, setValues ] = useState(initialState)
+    const onInputChange = ({ target }) => {
+        const { name, value } = target;
+        setFormState({
+            ...formState,
+            [ name ]: value
+        });
+    }
 
-    // Creamos una funcion que nos permita realizar cambios en nuestro formulario
-    const handleInputChange = ({ target }) => {
-        setValues({
-            ...values,
-            [target.name]: target.value
-        })
-    };
+    const onResetForm = () => {
+        setFormState( initialForm );
+    }
 
-    // Creamos una funcion que permita hacer el envio de la informacion con algun boton que configuremos, ... 
-    // ... ademas nos muestra la informacion guardada impresa en consola =D
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        
-        console.log(values);
-    };
-
-    // Creamos una funcion que resetea el campo del formulario una vez realizado el cambio
-    const reset = () => {
-        setValues( initialState )
-    };
-
-    // Importamos las funciones creadas de nuestro custom Hook
-    return [values, handleInputChange, reset, handleSubmit]
-
+    return {
+        ...formState,
+        formState,
+        onInputChange,
+        onResetForm,
+    }
 }
